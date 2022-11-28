171295693b9f9382428d0490b857104a22a77cc3
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-11-28 16:52:25 +0700

    Add case 1

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -73,6 +73,14 @@ namespace StackUsingSingleLinkList
                 Console.WriteLine("\nEnter your choice: ");
                 string Input = Console.ReadLine();
                 char ch = Convert.ToChar(Input == "" ? "0" : Input);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number :");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                }
             }
         }
     }
